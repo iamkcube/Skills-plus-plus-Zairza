485fd291e2e5d2aa312c9ce25a8fef9a3770e4a7
Kalinga <kalingakhatua@gmail.com>
2022-12-04 21:40:49 +0530

    finished javascript and css

---
+++ Task_2/js/script.js
@@ -0,0 +1,34 @@
+console.log('Hemlo');
+
+const cardHolder = document.querySelector('.latest-news__cards')
+// console.log(cardHolder);
+
+
+async function main() {
+    let data = await fetch('../assets/data.json')
+    let cards = await data.json()
+    // console.log(cards);
+
+    for (const card of cards.cards) {
+        let date = new Date(card.date).toDateString()
+        cardHolder.innerHTML += `
+            <div class="latest-news-card">
+                <img
+                    loading="lazy"
+                    src="${card.imageURL}"
+                    alt="Card Image"
+                    class="card-image"
+                />
+                <h2 class="card-title">${card.title}</h2>
+                <div class="card-details">
+                    <p class="card-date">${date}</p>
+                    <hr />
+                    <p class="card-comments">Comments (${card.comments})</p>
+                </div>
+            </div>
+        `
+    }
+    
+}
+
+main()
